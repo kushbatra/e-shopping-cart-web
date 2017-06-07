15fbe73bf6a5f8fe826e4f6d59ca04478c8d8977
Kush <kush.batra@gmail.com>
2017-06-07 08:50:23 -0400

    totalprice fix.

--- src/main/webapp/js/utility.js
+++ src/main/webapp/js/utility.js
@@ -53,10 +53,10 @@ var utility = {
 				var theTemplate = Handlebars.compile(template);
 				var theCompiledHtml = theTemplate(checkoutPage.cartData);
 				$(divId).html(theCompiledHtml);
-				if(path=='html/confirmation.html') {
-					$('#empty_cart').click();
-					checkoutPage.cartData.total=0;
-				}
+				//if(path=='html/confirmation.html') {
+				//	$('#empty_cart').click();
+				//	checkoutPage.cartData.total=0;
+				//}
 				checkoutPage.init();
 			}
 		});
